d9b0a0c75f9e182c731ad7a5aa13a52037fc1052
franklin <franklin@alphasoftz.in>
2026-01-09 23:08:15 +0530

    chnages

--- resources/views/frontend/product.blade.php
+++ resources/views/frontend/product.blade.php
@@ -987,7 +987,7 @@
                                         <div class="product-details">
                                             <div class="product-cat"><a href="shop-banner-sidebar.html">{{  $prouctsList['shop_name'] }}</a>
                                             </div>
-                                            <h4 class="product-name"><a href="product-default.html">{{ $product->product_name }}</a>
+                                            <h4 class="product-name"><a href="{{ url('productVar').'/'.$product->id }}">{{ $product->product_name }}</a>
                                             </h4>
                                             <div class="ratings-container">
                                                 <div class="ratings-full">
@@ -997,13 +997,13 @@
                                                 <a href="product-default.html" class="rating-reviews">(3 reviews)</a>
                                             </div>
                                               <div class="product-pa-wrapper">
-                <div class="product-price">₹{{ $product->selling_price }}</div>
-                <div class="product-price-discount">₹{{ $product->retail_price }}</div>
-                @php
-                    $discount = number_format((($product->retail_price - $product->selling_price) / $product->retail_price) * 100);
-                @endphp
-                <div class="product-offer-percentage">{{ $discount }}% Off</div>
-            </div>
+                                                    <div class="product-price">₹{{ $product->selling_price }}</div>
+                                                    <div class="product-price-discount">₹{{ $product->retail_price }}</div>
+                                                    @php
+                                                        $discount = number_format((($product->retail_price - $product->selling_price) / $product->retail_price) * 100);
+                                                    @endphp
+                                                    <div class="product-offer-percentage">{{ $discount }}% Off</div>
+                                                </div>
                                         </div>
                                     </div>
                                 @endforeach
@@ -1018,7 +1018,7 @@
                              <a href="#" class="btn btn-dark btn-link btn-slide-right btn-icon-right">More
                                  Products<i class="w-icon-long-arrow-right"></i></a>
                          </div>
-                         <div class="swiper-container swiper-theme" data-swiper-options="{
+                            <div class="swiper-container swiper-theme" data-swiper-options="{
                                     'spaceBetween': 20,
                                     'slidesPerView': 2,
                                     'breakpoints': {
@@ -1033,154 +1033,48 @@
                                         }
                                     }
                                 }">
-                             <div class="swiper-wrapper row cols-lg-3 cols-md-4 cols-sm-3 cols-2">
-                                 <div class="swiper-slide product">
-                                     <figure class="product-media">
-                                         <a href="product-default.html">
-                                             <img src="<?php echo asset('assets') ?>/images/products/default/5.jpg" alt="Product"
-                         />
-                                         </a>
-                                         <div class="product-action-vertical">
-                                             <a href="#" class="btn-product-icon btn-cart w-icon-cart"
-                                                 title="Add to cart"></a>
-                                             <a href="#" class="btn-product-icon btn-wishlist w-icon-heart"
-                                                 title="Add to wishlist"></a>
-                                             <a href="#" class="btn-product-icon btn-compare w-icon-compare"
-                                                 title="Add to Compare"></a>
-                                         </div>
-                                         <div class="product-action">
-                                             <a href="#" class="btn-product btn-quickview" title="Quick View">Quick
-                                                 View</a>
-                                         </div>
-                                     </figure>
-                                     <div class="product-details">
-                                         <h4 class="product-name"><a href="product-default.html">Drone</a></h4>
-                                         <div class="ratings-container">
-                                             <div class="ratings-full">
-                                                 <span class="ratings" style="width: 100%;"></span>
-                                                 <span class="tooltiptext tooltip-top"></span>
-                                             </div>
-                                             <a href="product-default.html" class="rating-reviews">(3 reviews)</a>
-                                         </div>
-                                           <div class="product-pa-wrapper">
-                                                <div class="product-price">₹{{ $product->selling_price }}</div>
-                                                    <div class="product-price-discount">₹{{ $product->retail_price }}</div>
-                                                    @php
-                                                        $discount = number_format((($product->retail_price - $product->selling_price) / $product->retail_price) * 100);
-                                                    @endphp
-                                                    <div class="product-offer-percentage">{{ $discount }}% Off</div>
+                                <div class="swiper-wrapper row cols-lg-3 cols-md-4 cols-sm-3 cols-2">
+                                    @foreach ($vendorProducts as $product)
+                                        <div class="swiper-slide product">
+                                            <figure class="product-media">
+                                                <a href="{{ url('productVar').'/'.$product->id }}">
+                                                    <img src="{{ asset('assets/images/products/'.$product->product_image) }}" alt="Product"/>
+                                                </a>
+                                                <div class="product-action-vertical">
+                                                    <a href="#" class="btn-product-icon btn-cart w-icon-cart"
+                                                        title="Add to cart"></a>
+                                                    <a href="#" class="btn-product-icon btn-wishlist w-icon-heart"
+                                                        title="Add to wishlist"></a>
+                                                  
                                                 </div>
+                                                <div class="product-action">
+                                                    <a   onclick="showQuickView('{{ $product->id }}')" data-id="{{ $product->id }}" class="btn-product btn-quickview" title="Quick View">Quick
+                                                        View</a>
+                                                </div>
+                                            </figure>
+                                            <div class="product-details">
+                                                <h4 class="product-name"><a href="{{ url('productVar').'/'.$product->id }}">{{ $product->product_name }}</a></h4>
+                                                <div class="ratings-container">
+                                                    <div class="ratings-full">
+                                                        <span class="ratings" style="width: 100%;"></span>
+                                                        <span class="tooltiptext tooltip-top"></span>
+                                                    </div>
+                                                    <a href="product-default.html" class="rating-reviews">(3 reviews)</a>
+                                                </div>
+                                                <div class="product-pa-wrapper">
+                                                        <div class="product-price">₹{{ $product->selling_price }}</div>
+                                                            <div class="product-price-discount">₹{{ $product->retail_price }}</div>
+                                                            @php
+                                                                $discount = number_format((($product->retail_price - $product->selling_price) / $product->retail_price) * 100);
+                                                            @endphp
+                                                            <div class="product-offer-percentage">{{ $discount }}% Off</div>
+                                                        </div>
                                             </div>
-                                 </div>
-                                 <div class="swiper-slide product">
-                                     <figure class="product-media">
-                                         <a href="product-default.html">
-                                             <img src="<?php echo asset('assets') ?>/images/products/default/6.jpg" alt="Product"
-                                                 width="300" height="338" />
-                                         </a>
-                                         <div class="product-action-vertical">
-                                             <a href="#" class="btn-product-icon btn-cart w-icon-cart"
-                                                 title="Add to cart"></a>
-                                             <a href="#" class="btn-product-icon btn-wishlist w-icon-heart"
-                                                 title="Add to wishlist"></a>
-                                             <a href="#" class="btn-product-icon btn-compare w-icon-compare"
-                                                 title="Add to Compare"></a>
-                                         </div>
-                                         <div class="product-action">
-                                             <a href="#" class="btn-product btn-quickview" title="Quick View">Quick
-                                                 View</a>
-                                         </div>
-                                     </figure>
-                                     <div class="product-details">
-                                         <h4 class="product-name"><a href="product-default.html">Official Camera</a>
-                                         </h4>
-                                         <div class="ratings-container">
-                                             <div class="ratings-full">
-                                                 <span class="ratings" style="width: 100%;"></span>
-                                                 <span class="tooltiptext tooltip-top"></span>
-                                             </div>
-                                             <a href="product-default.html" class="rating-reviews">(3 reviews)</a>
-                                         </div>
-                                         <div class="product-pa-wrapper">
-                                             <div class="product-price">
-                                                 <ins class="new-price">$263.00</ins><del
-                                                     class="old-price">$300.00</del>
-                                             </div>
-                                         </div>
-                                     </div>
-                                 </div>
-                                 <div class="swiper-slide product">
-                                     <figure class="product-media">
-                                         <a href="product-default.html">
-                                             <img src="<?php echo asset('assets') ?>/images/products/default/7-1.jpg" alt="Product"
-                                                 width="300" height="338" />
-                                             <img src="<?php echo asset('assets') ?>/images/products/default/7-2.jpg" alt="Product"
-                                                 width="300" height="338" />
-                                         </a>
-                                         <div class="product-action-vertical">
-                                             <a href="#" class="btn-product-icon btn-cart w-icon-cart"
-                                                 title="Add to cart"></a>
-                                             <a href="#" class="btn-product-icon btn-wishlist w-icon-heart"
-                                                 title="Add to wishlist"></a>
-                                             <a href="#" class="btn-product-icon btn-compare w-icon-compare"
-                                                 title="Add to Compare"></a>
-                                         </div>
-                                         <div class="product-action">
-                                             <a href="#" class="btn-product btn-quickview" title="Quick View">Quick
-                                                 View</a>
-                                         </div>
-                                     </figure>
-                                     <div class="product-details">
-                                         <h4 class="product-name"><a href="product-default.html">Phone Charge Pad</a>
-                                         </h4>
-                                         <div class="ratings-container">
-                                             <div class="ratings-full">
-                                                 <span class="ratings" style="width: 80%;"></span>
-                                                 <span class="tooltiptext tooltip-top"></span>
-                                             </div>
-                                             <a href="product-default.html" class="rating-reviews">(8 reviews)</a>
-                                         </div>
-                                         <div class="product-pa-wrapper">
-                                             <div class="product-price">$23.00</div>
-                                         </div>
-                                     </div>
-                                 </div>
-                                 <div class="swiper-slide product">
-                                     <figure class="product-media">
-                                         <a href="product-default.html">
-                                             <img src="<?php echo asset('assets') ?>/images/products/default/8.jpg" alt="Product"
-                                                 width="300" height="338" />
-                                         </a>
-                                         <div class="product-action-vertical">
-                                             <a href="#" class="btn-product-icon btn-cart w-icon-cart"
-                                                 title="Add to cart"></a>
-                                             <a href="#" class="btn-product-icon btn-wishlist w-icon-heart"
-                                                 title="Add to wishlist"></a>
-                                             <a href="#" class="btn-product-icon btn-compare w-icon-compare"
-                                                 title="Add to Compare"></a>
-                                         </div>
-                                         <div class="product-action">
-                                             <a href="#" class="btn-product btn-quickview" title="Quick View">Quick
-                                                 View</a>
-                                         </div>
-                                     </figure>
-                                     <div class="product-details">
-                                         <h4 class="product-name"><a href="product-default.html">Fashionalble
-                                                 Pencil</a></h4>
-                                         <div class="ratings-container">
-                                             <div class="ratings-full">
-                                                 <span class="ratings" style="width: 100%;"></span>
-                                                 <span class="tooltiptext tooltip-top"></span>
-                                             </div>
-                                             <a href="product-default.html" class="rating-reviews">(9 reviews)</a>
-                                         </div>
-                                         <div class="product-pa-wrapper">
-                                             <div class="product-price">$50.00</div>
-                                         </div>
-                                     </div>
-                                 </div>
-                             </div>
-                         </div>
+                                        </div>
+                                    
+                                    @endforeach
+                                </div>
+                            </div>
                      </section>
                  </div>
                  <!-- End of Main Content -->
