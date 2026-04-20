c596d904e9a7591e8bcc1c9e5870f2935a60cba1
franklin <franklin@alphasoftz.in>
2026-04-20 17:53:24 +0530

    changes

--- resources/views/paritials/css/login-css.blade.php
+++ resources/views/paritials/css/login-css.blade.php
@@ -1,26 +1,6 @@
 <style>
-    body {
-        background-color: #f4f7f6;
-    }
-
-    .page-wrapper {
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        min-height: 100vh;
-        width: 100%;
-    }
-
-    .authentication-box {
-        width: 100%;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-    }
-    
-    @media (max-width: 767px) {
-        .page-wrapper {
-            padding: 20px 0;
-        }
-    }
+body
+{
+background-color: #f4f7f6;
+}
 </style>
