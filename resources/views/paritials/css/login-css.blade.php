62df0a7706b29687d4d6ec6a4594a1716bf3ca72
franklin <franklin@alphasoftz.in>
2026-05-27 16:39:04 +0530

    changes

--- resources/views/paritials/css/login-css.blade.php
+++ resources/views/paritials/css/login-css.blade.php
@@ -1,6 +1,17 @@
 <style>
-body
-{
-background-color: #f4f7f6;
+body {
+    background-color: #f4f7f6;
+}
+
+/* Fix for mobile webview keyboard overlap issue where form gets pushed to the top and cut off */
+.authentication-box {
+    align-items: flex-start !important;
+}
+
+.authentication-box .container {
+    margin-top: auto !important;
+    margin-bottom: auto !important;
+    padding-top: 30px;
+    padding-bottom: 30px;
 }
 </style>
