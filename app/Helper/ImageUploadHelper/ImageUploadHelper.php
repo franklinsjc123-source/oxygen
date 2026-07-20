dc90d90c4246c4808030a19866bf1727888da36e
franklin <franklin@alphasoftz.in>
2026-07-20 20:02:54 +0530

    canges

--- app/Helper/ImageUploadHelper/ImageUploadHelper.php
+++ app/Helper/ImageUploadHelper/ImageUploadHelper.php
@@ -8,8 +8,11 @@ class ImageUploadHelper
 
     public static function storeImage($file, string $path)
     {
+        if (empty($file) || !is_object($file) || !method_exists($file, 'getClientOriginalExtension')) {
+            return '';
+        }
 
-        $imageName = time() . '.' . $file->getClientOriginalExtension();
+        $imageName = time() . '_' . uniqid() . '.' . $file->getClientOriginalExtension();
         $destinationPath = public_path($path);
         $file->move($destinationPath, $imageName);
         $file->imagePath = $destinationPath . $imageName;
